8edb22a0160b7be809c93d5918c936166bdad6ad
Milo <milo@Mac-mini-von-Milo.local>
2025-11-10 15:36:43 +0100

    Initial commit

---
+++ src/main/kotlin/net/milocodee/minigame/MiniGamePlugin.kt
@@ -0,0 +1,14 @@
+package net.milocodee.minigame
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MiniGamePlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/net/milocodee/minigame/util/PlayerState.kt
@@ -0,0 +1,4 @@
+package net.milocodee.minigame.util
+
+class PlayerState {
+}
